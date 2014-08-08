8ffe99d14145c3f7b979a1f42ad8b5a43a033e35
Brian Christian <brchristian@gmail.com>
2014-08-07 17:57:12 -0700

    information about subscription status in renewal page

--- app/overrides/add_to_cart_form.rb
+++ app/overrides/add_to_cart_form.rb
@@ -5,6 +5,15 @@ Deface::Override.new(virtual_path: "spree/products/_cart_form",
   )
 
 Deface::Override.new(virtual_path: "spree/products/_cart_form",
+  name: "add_subscription_info_to_add_to_cart",
+  insert_bottom: "div.add-to-cart",
+  text: "<% if @product.subscribable? && spree_current_user && spree_current_user.subscriptions.where(magazine: @product).present? %>
+  <% user_sub = spree_current_user.subscriptions.where(magazine: @product).first %>
+  <div class='subscription_status'><%= user_sub.remaining_issues > 0 ? user_sub.remaining_issues.to_s + ' ' + 'Issue'.pluralize(user_sub.remaining_issues) + ' Remaining' : 'Expired on ' + user_sub.shipped_issues.last.issue.shipped_at.strftime('%B %e, %Y') %></div>
+  <% end %>"
+  )
+
+Deface::Override.new(virtual_path: "spree/products/_cart_form",
   name: "hide_number_field_for_subscribable_products",
   replace: "erb[loud]:contains(\"number_field_tag\")",
   partial: "spree/products/cart_form_number_field"
